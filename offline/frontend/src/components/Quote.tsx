e07d5f1bb664c03ac340172962161396b294f985
ajaychelikhani <ajaychelikhani@gmail.com>
2024-10-20 12:40:39 +0530

    Updated whole repo with separate folder for frontend,backend,common

---
+++ offline/frontend/src/components/Quote.tsx
@@ -0,0 +1,17 @@
+export const Quote=()=>{
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className=" text-3xl font-bold">
+                    "The customer support I received was exceptional. The support team went and beyond to address my concerns"
+                </div>
+                <div className="max-w-lg  text-xl font-semibold text-left mt-4">
+                    Julies Winfield
+                </div>
+                <div className="max-w-lg  text-sm font-semibold">
+                    CEO | Acme corp
+                </div>
+            </div>
+        </div>
+    </div>
+}
---
+++ offline/frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL="https://backend.writeandshare.workers.dev"
